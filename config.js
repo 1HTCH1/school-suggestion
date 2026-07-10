00f6caeec829b21ca51ba56e0e78685143258690
1HTCH1 <9pc6h62d6g@privaterelay.appleid.com>
2026-07-10 10:18:20 +0900

    config.js 업데이트

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 // 프로젝트 값으로 바꿔주세요.
 // ============================================
 
-const SUPABASE_URL = "여기에_Project_URL_붙여넣기";
-const SUPABASE_ANON_KEY = "여기에_anon_public_key_붙여넣기";
+const SUPABASE_URL = "https://akfdxhadezadahnjziqh.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrZmR4aGFkZXphZGFobmp6aXFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MjA1MjUsImV4cCI6MjA5OTE5NjUyNX0.ITaQyOsgL5RccKm5B3ESKbgUt7ifn7qgBRYs3k4kaNc";
 
 // 아래는 수정할 필요 없음
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
